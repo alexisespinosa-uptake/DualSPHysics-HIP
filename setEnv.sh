5ad7cf031ab7a677ab34559895956568124c2d73
Alexis Espinosa <Alexis.Espinosa@pawsey.org.au>
2025-05-20 11:51:14 +0800

    New compilation environment

--- setEnv.sh
+++ setEnv.sh
@@ -4,7 +4,10 @@
 case "$HOSTNAME" in
     *setonix*)
 	echo "Setting environment for Setonix"
-        module load rocm/5.6.1
+	module load cpe/24.07
+	module swap pawseyenv/2024.05 pawseyenv/2025.03
+        module load gcc-native/13.2
+        module load rocm/6.2.4
         module load craype-accel-amd-gfx90a
 	module list
         ;;
